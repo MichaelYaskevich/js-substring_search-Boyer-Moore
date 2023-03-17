83d84fc452e6d75f3f1044855b01dd7f67ac8ca8
MichaelYaskevich <The_person38@mail.ru>
2023-03-17 13:18:40 +0500

    Вынес мусор

--- src/bm.js
+++ src/bm.js
@@ -1,58 +1,52 @@
 ﻿let fs = require('fs');
 let arg = process.argv;
-let k;
-let str;
-let char;
-let subStr;
-let strLen;
-let subStrLen;
-let N;
-let i;
-let array;
-let j;
-let rpr;
+let result;
 
-function read(inFile) {
+
+function read(input_file) {
     try {
-        inText = fs.readFileSync(inFile, "utf-8");
-        return inText;
+        return fs.readFileSync(input_file, "utf-8");
     }
     catch (e) {
         return undefined;
     }
 }
 
-function MakeTableN(subStr) {
-    subStrLen = subStr.length;
-    N = new Array();
-    for (j = 0; j < subStrLen; j++)
-        N[subStr.charAt(j)] = j+1;
-    return N;
+function makeRightmostOccurancesTable(substr) {
+    // Таблица крайних правых входений символов в подстроку makeRightmostOccurancesTable
+    let substrLen = substr.length;
+    let table = new Array();
+    for (let j = 0; j < substrLen; j++)
+        table[substr.charAt(j)] = j+1;
+    return table;
 }
 
-function Check(subStr, k, l, subStrLen) {
-    let j = 0; //T*[k..k + l − 1] = T[m − l + 1..m];
-    while (k + l - 2 - j >= 0
-        && subStr.charAt(k + l - 2 - j) == subStr.charAt(subStrLen - 1 - j)) {
+function check(substr, k, match_len, substr_len) {
+    // Проверяет условие T*[k..k + l − 1] = T[m − l + 1..m], где T* = AT, где A - последовательность любых символов (*) длины |T|.
+    // И условие T[k-1] != T[m-l] при k > 1.
+    let j = 0;
+    let right = k + match_len - 2
+    while (right - j >= 0
+        && substr.charAt(right - j) == substr.charAt(substr_len - 1 - j)) {
         j++;
     }
-    if (k + l - 2 - j < 0 || j >= l)
+    if ((right - j < 0 || j >= match_len) // Если right - j < 0, то далее один из участников сравнения будет *, что приведет к равенству.
+        && (k > 1 && substr.charAt(k - 2) != substr.charAt(substr_len - match_len - 1) || k <= 1))
         return true;
     else
         return false;
 }
 
-function MakeTableRpr(subStr) {
-    subStrLen = subStr.length;
-    rpr = new Array(subStrLen);
-    for (let l = 0; l <= subStrLen; l++) {
-        k = subStrLen - l;
+function makeRprTable(substr) {
+    // Создает rightmost plausible reoccurrence (rpr) таблицу.
+    let substr_len = substr.length;
+    let rpr = new Array(substr_len);
+    for (let match_len = 0; match_len <= substr_len; match_len++) {
+        let k = substr_len - match_len;
         while (true) {
-            if (Check(subStr, k, l, subStrLen)) {
-                if (k > 1 && subStr.charAt(k - 2) != subStr.charAt(subStrLen - l - 1) || k <= 1) {
-                    rpr[l] = k;
-                    break;
-                }
+            if (check(substr, k, match_len, substr_len)) {
+                rpr[match_len] = k;
+                break;
             }
             k--;
         }
@@ -60,80 +54,83 @@ function MakeTableRpr(subStr) {
     return rpr;
 }
 
-function Shift1(subStrLen, N, char, l) {
-    if (N[char] == undefined)
-        return Math.max(subStrLen - l, 1);
-    return Math.max(subStrLen - N[char] - l, 1);
+function getShift1(substr_len, ro_table, char, match_len) {
+    return ro_table[char] == undefined
+        ? Math.max(substr_len - match_len, 1)
+        : Math.max(substr_len - ro_table[char] - match_len, 1);
 }
 
-function Shift2(subStrLen, rpr, l) {
-    return subStrLen - rpr[l] - l + 1;
+function getShift2(substr_len, rpr, match_len) {
+    return substr_len - rpr[match_len] - match_len + 1;
 }
 
-function bm(strFile, substrFile) {
-    str = read(strFile);
-    if (str == undefined || str == "")
-        return undefined;
-    subStr = read(substrFile);
-    if (subStr == undefined || subStr == "")
-        return undefined;
-    strLen = str.length;
-    subStrLen = subStr.length;
-    array = new Array();
-    i = 0;
+function boyerMooreAlgo(str, substr) {
+    // Ищет подстроку в строке алгоритмом Бойера Мура.
+    let str_len = str.length;
+    let substr_len = substr.length;
+    let result = new Array();
+    let i = 0;
+    
     const start = new Date().getTime();
-    N = MakeTableN(subStr);
-    rpr = MakeTableRpr(subStr);
-    while (i < strLen - subStrLen + 2) {
-        j = subStr.length - 1;
-        while (str[i + j] == subStr[j]) {
+    let ro_table = makeRightmostOccurancesTable(substr);
+    let rpr_table = makeRprTable(substr);
+    while (i < str_len - substr_len + 2) {
+        let j = substr.length - 1;
+        while (str[i + j] == substr[j]) {
             j--;
             if (j == -1) {
-                array[array.length] = i;
+                result[result.length] = i;
                 break;
             }
         }
-        if (howManyEntriesShow != undefined && array.length == howManyEntriesShow)
+        if (count_of_entries != undefined && result.length == count_of_entries)
             break;
-        l = subStr.length - 1 - j;
-        char = str.charAt(i + j);
-        i += Math.max(Shift1(subStrLen, N, char, l), Shift2(subStrLen, rpr, l));
+        let match_len = substr.length - 1 - j;
+        let char = str.charAt(i + j);
+        i += Math.max(
+            getShift1(substr_len, ro_table, char, match_len), 
+            getShift2(substr_len, rpr_table, match_len));
     }
     const end = new Date().getTime();
 
     if (time == true) console.log(`WorkTime: ${end - start}ms`);
 
-    return array;
+    return result;
 }
 
-let flagsCount = 0;
-let howManyEntriesShow;
+let flags_count = 0;
+let count_of_entries;
 let time = false;
-function CheckFlags() {
-    if (arg[2 + flagsCount] == "-n") {
-        howManyEntriesShow = arg[2 + flagsCount + 1];
-        flagsCount++;
-        flagsCount++;
-        CheckFlags();
+function checkFlags() {
+    if (arg[2 + flags_count] == "-n") {
+        count_of_entries = arg[2 + flags_count + 1];
+        flags_count++;
+        flags_count++;
+        checkFlags();
     }
-    else if (arg[2 + flagsCount] == "-t") {
-        flagsCount++;
+    else if (arg[2 + flags_count] == "-t") {
+        flags_count++;
         time = true;
-        CheckFlags();
+        checkFlags();
     }
 }
-CheckFlags();
+checkFlags();
 
-if (howManyEntriesShow == undefined || howManyEntriesShow > 0) {
-    array = bm(arg[2 + flagsCount], arg[3 + flagsCount]);
+if (count_of_entries == undefined || count_of_entries > 0) {
+    let str = read(arg[2 + flags_count]);
+    let substr = read(arg[3 + flags_count]);
+    if (str == undefined || str == "" || substr == undefined || substr == "")
+        result = undefined;
+    else
+        result = boyerMooreAlgo(str, substr);
 
-    if (array == undefined) console.log("One of the files is empty or not exit");
+    if (result == undefined) console.log("One of the files is empty or not exit");
     else {
         console.log("------Entries-----");
-        if (howManyEntriesShow == undefined || howManyEntriesShow > array.length)
-            howManyEntriesShow = array.length;
-        for (let i = 0; i < howManyEntriesShow; i++) {
-            console.log(`|            ${array[i]}           |`);
+        if (count_of_entries == undefined || count_of_entries > result.length)
+            count_of_entries = result.length;
+        for (let i = 0; i < count_of_entries; i++) {
+            console.log(`|            ${result[i]}           |`);
         }
         console.log("--------------------------");
     }
